39786436069fd3d4e808a659b4b9fc6234b82250
Austin G. Davis-Richardson <harekrishna@gmail.com>
2012-01-27 16:10:22 -0500

    rspecs now use actual data files for testing

--- spec/dna/dna_spec.rb
+++ spec/dna/dna_spec.rb
@@ -1,33 +1,11 @@
 require 'spec_helper'
 
 describe Dna do
+	include_context "parser stuff"
 	
-	let(:fasta) {
-		(">testtest\nGATCAGAGATCAT\ngacctatcaca\nGAGAGATANANANAGACTATA\n" + \
-		"\n\n>testtesttest\nGAGAGACACGAG\n\nGAGAGCACANANANAN\n").split
-	}
-	
-	let (:fastq) {
-	  f=<<-EOF
-@1UOLP:4:17
-TGAAACTTATTGATCACCCCGCTTGGCGTTGGGGAGAAATTCAGAAAAGAGTGCTTGATGGGGCGCCACATGCCGTGCAACCCACTCTCTTTCACGCAGCGCGCCCCA
-+
-5888.6778888650/-//&,(,./*-11'//0&,-0.(.,,,,/2/&-,,,,,.(.,(,..&---&-,,,((*-----*+.&,,,,,(//&,,,-(,,+(,,,--&(
-@1UOLP:4:32
-GTCGCGGCTTACCACCCAACGATTTTTTTTAGAGGTGCTGGTTTCA
-+
-2550//*-1./4.--/'+.2.,,,,,,,,&(/00.11426554+13
-	  EOF
-	  f.split
-	}
-	
-	let (:qseq) {
-	  f=<<-EOF
-HWUSI-EAS1690	0007	1	1	1139	20892	0	1	GTGTGCCAGCCGCCGCGGTAATACGTAGGTGGCAAGCGTTGTCCGGATTTATTGGGTGTAAAGGGCGCGTAGGCGGCCCTGTAAGTCAGTGGTGAAATCTC	fffffffffffffffeeeeedddddabdd\dddabeeeee^aabdcabddaKdddc`RcY`_c`aT`Ib]Tc^\cZEKOZ_\]\bZVK^UZG]`[^BBBBB	1
-HWUSI-EAS1690	0007	1	1	1174	18551	0	1	GTGTGCCAGCAGCCGCGGTAATACAGAGGGGGCAAGCGTTGTTCGGAATTACTGGGCGTAAAGGGCGCGTAGGCGGCCCGCTAAGCCGAACGTGAAATCCC	ffffffffffcffffffeffddddYdb^ddbJ]Y^eeede^eadeedLeed\ddddIdddd`ddd^edcTacaa`aJ^aLZ^]Y^BBBBBBBBBBBBBBBB	1
-  EOF
-  f.split("\n")
-	}
+	let(:fasta) { @fasta_file }
+	let (:fastq) { @fastq_file }
+	let (:qseq) { @qseq_file }
 	
 	it 'can parse a crappily formatted fasta file' do
 		records = Dna.new fasta, type: 'fasta'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,16 @@ $LOAD_PATH << File.join(File.dirname(__FILE__), 'lib')
 
 require 'rspec'
 require 'dna'
+
+path = File.dirname(__FILE__)
+fasta_file = File.readlines(File.join(path, 'data/test.fasta'))
+fastq_file = File.readlines(File.join(path, 'data/test.fastq'))
+qseq_file = File.readlines(File.join(path, 'data/test.qseq'))
+
+shared_context "parser stuff" do
+  before do
+    @fasta_file = fasta_file
+    @fastq_file = fastq_file
+    @qseq_file = qseq_file
+  end
+end
