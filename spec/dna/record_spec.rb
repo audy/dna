ca462304ab6ca8cdab09d6ed22d0e6a38b8c3108
Austin G. Davis-Richardson <harekrishna@gmail.com>
2012-01-27 17:45:16 -0500

    more tests

--- spec/dna/record_spec.rb
+++ spec/dna/record_spec.rb
@@ -46,10 +46,11 @@ describe Fastq do
 end
 
 describe QSEQ do
-  let (:qseq) {
-    line = "HWUSI-EAS1690\t0007\t1\t1\t1139\t20892\t0\t1\tGTGTGCCAGCCGCCGCGGTAATACGTAGGTGGCAAGCGTTGTCCGGATTTATTGGGTGTAAAGGGCGCGTAGGCGGCCCTGTAAGTCAGTGGTGAAATCTC\tfffffffffffffffeeeeedddddabdd\dddabeeeee^aabdcabddaKdddc`RcY`_c`aT`Ib]Tc^\cZEKOZ_\]\bZVK^UZG]`[^BBBBB\t1".split("\t")
-    QSEQ.new(
-      machine: line[0],
+  let (:string) { "HWUSI-EAS1690\t0007\t1\t1\t1139\t20892\t0\t1\tGTGTGCCAGCCGCCGCGGTAATACGTAGGTGGCAAGCGTTGTCCGGATTTATTGGGTGTAAAGGGCGCGTAGGCGGCCCTGTAAGTCAGTGGTGAAATCTC\tfffffffffffffffeeeeedddddabdd\dddabeeeee^aabdcabddaKdddc`RcY`_c`aT`Ib]Tc^\cZEKOZ_\]\bZVK^UZG]`[^BBBBB\t1" }
+
+  let (:properties) {
+    line = string.split("\t")
+    { machine: line[0],
       run: line[1],
       lane: line[2],
       tile: line[3],
@@ -59,20 +60,27 @@ describe QSEQ do
       read_no: line[7],
       sequence: line[8],
       quality: line[9],
-      filtered: line[10],
-  )}
-  
+      filtered: line[10] }
+  }
+
+  let (:qseq) {
+    QSEQ.new(properties)}
+
   it 'can be created' do
     qseq.should_not be_nil
   end
-  
+
   it 'has a header' do
     qseq.header.should_not be_nil
   end
-  
+
   ['machine', 'run', 'lane', 'tile', 'x', 'y', 'index', 'read_no', 'sequence', 'quality', 'filtered'].each do |property|
     it "has property #{property}" do
-      qseq.send(property).should_not be_nil
+      qseq.send(property).should == properties[property.to_sym]
     end
+  end
+
+  it 'can be printed back in its original form' do
+    qseq.to_s.should == string
   end
 end
