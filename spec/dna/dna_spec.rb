ad8c46757abc1d0be4f5a0d3ded09be0c8a92cb9
Austin G. Davis-Richardson <harekrishna@gmail.com>
2012-01-27 17:06:59 -0500

    frakkin tabs?!

--- spec/dna/dna_spec.rb
+++ spec/dna/dna_spec.rb
@@ -1,19 +1,19 @@
 require 'spec_helper'
 
 describe Dna do
-	include_context "parser stuff"
-	
-	let(:fasta) { @fasta_file }
-	let (:fastq) { @fastq_file }
-	let (:qseq) { @qseq_file }
-	
-	it 'can parse a crappily formatted fasta file' do
-		records = Dna.new fasta, type: 'fasta'
+  include_context "parser stuff"
+  
+  let(:fasta) { @fasta_file }
+  let (:fastq) { @fastq_file }
+  let (:qseq) { @qseq_file }
+  
+  it 'can parse a crappily formatted fasta file' do
+    records = Dna.new fasta, type: 'fasta'
     records.to_a.length.should == 2
-	end
-	
-	it 'can parse fastq format' do
-	  records = Dna.new fastq, type: 'fastq'
+  end
+  
+  it 'can parse fastq format' do
+    records = Dna.new fastq, type: 'fastq'
     records.to_a.length.should == 2
   end
   
