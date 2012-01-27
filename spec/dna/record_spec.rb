dd8971f9901f07d77e3661e8ebf7d23050d6f576
Austin G. Davis-Richardson <harekrishna@gmail.com>
2012-01-27 17:04:39 -0500

    tabs->spaces

--- spec/dna/record_spec.rb
+++ spec/dna/record_spec.rb
@@ -1,23 +1,23 @@
 require 'spec_helper'
 
 describe Fasta do
-	
-	let (:fasta) { Fasta.new name: 'test', sequence: 'GATC'}
-	
-	it 'can be created' do
-		fasta.should_not be_nil
-	end
-	
-	it 'has a name' do
-		fasta.name.should_not be_nil
-	end
-	
-	it 'has a sequence' do
-		fasta.sequence.should_not be_nil
-	end
-	
-	it 'has a length' do
-	  fasta.length.should == fasta.sequence.length
+  
+  let (:fasta) { Fasta.new name: 'test', sequence: 'GATC'}
+  
+  it 'can be created' do
+    fasta.should_not be_nil
+  end
+  
+  it 'has a name' do
+    fasta.name.should_not be_nil
+  end
+  
+  it 'has a sequence' do
+    fasta.sequence.should_not be_nil
+  end
+  
+  it 'has a length' do
+    fasta.length.should == fasta.sequence.length
   end
 end
 
